ff91d2fa53d6b98e87a53a8ace229e5c64e1a5e1
Alice Lemerre <al.lemerre@laposte.net>
2024-06-02 22:55:04 +0200

    ajout compposants et styles

---
+++ rendu-conditionnel/src/App.jsx
@@ -0,0 +1,11 @@
+import Articles from './components/Articles';
+
+function App() {
+  return (
+    <div className="d-flex flex-row justify-content align-items-center p-20">
+      <Articles />
+    </div>
+  );
+}
+
+export default App;
